166f487995f1597e02dfe189066a2eb72b19a948
Michelle Jordan <tmsmith1150@live.com>
2024-07-26 17:03:34 -0400

    deploy

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,6 +5,12 @@ import logo from '@/assets/images/logo.png';
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
+
+    const myCopyrightStyle = {
+      color: "#e6021d",
+      hover: "#03fc4e"
+    }
+
   return (
     // <!-- Footer -->
     <footer className="bg-gray-200 py-4 mt-48">
@@ -14,20 +20,20 @@ const Footer = () => {
         <div className="mb-4 md:mb-0">
           <Image src={ logo } alt="Logo" className="h-8 w-auto" />
         </div>
+        <div>
+          <p className="text-sm text-gray-500 mt-2 md:mt-0">
+            &copy; {currentYear} All rights reserved || Powered by: <span className='crlogo' style={myCopyrightStyle}><Link href="https://www.mojowebsitedesignanddevelopment.com/">MOJOWEBSITEDESIGNANDDEVELOPMENT</Link></span>
+          </p>
+        </div>
         <div
           className="flex flex-wrap justify-center md:justify-start mb-4 md:mb-0"
         >
           <ul className="flex space-x-4">
-            <li><Link href="/products">Shop All Products</Link></li>
             {/* create terms component */}
             <li><Link href="/terms">Terms of Service</Link></li>
           </ul>
         </div>
-        <div>
-          <p className="text-sm text-gray-500 mt-2 md:mt-0">
-            &copy; {currentYear} PropertyPulse. All rights reserved.
-          </p>
-        </div>
+        
       </div>
     </footer>
   )
